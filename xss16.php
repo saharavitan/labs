947a6fdefe8958aed66c758d1c060eb3c03da9bf
Sahar Avitan <avitansahar@gmail.com>
2022-12-05 05:17:23 -0800

    aa

--- xss16.php
+++ xss16.php
@@ -41,29 +41,49 @@ include("up.php");
                 what is your middle name ? <br><br> <input type="text" name="middle" value="" placeholder="dude">
                 <br>
                 what is your age ? <br><br> <input type="text" name="age" value="" placeholder="29">
+                <br>
+                what is your id ? <br><br> <input type="text" name="id" value="" placeholder="29">
 
                 <input type="submit" value="ok">
             </form>
             <br>
 
+            <?php
+
+            $first = str_replace("<", "", $_GET["first"]);
+            $last = str_replace("<", "", $_GET["last"]);
+            $middle = str_replace("<", "", $_GET["middle"]);
+            $age = str_replace("<", "", $_GET["age"]);
+            $id = str_replace("<", "", $_GET["id"]);
+
+            $first = str_replace(">", "", $first);
+            $last = str_replace(">", "", $last);
+            $middle = str_replace(">", "", $middle);
+            $age = str_replace(">", "", $age);
+            $id = str_replace(">", "", $id);
+
+            ?>
+            <script>
+                var name = "<?= $first ?>";
+            </script>
 
             <script>
-                var name = "<?= $_GET["first"] ?>";
+                var name = <?= $id ?>;
             </script>
 
             <script>
                 var name = "aaaaaaaaa";
-                console.log("<?= $_GET["last"] ?>" + name);
+                console.log("<?= $last ?>" + name);
                 var tmp = "";
             </script>
 
             <script>
-                const x = () => {console.log("<?= $_GET["middle"] ?>");}
+                const x = () => {console.log("<?= $middle ?>");}
                 x();
             </script>
 
             <script>
-                var json_temp = {"first": "<?= htmlentities($_GET["first"], ENT_QUOTES, 'UTF-8') ?>", "age":<?= $_GET["age"] ?>, "middle":"<?= htmlentities($_GET["middle"], ENT_QUOTES, 'UTF-8') ?>", "last":"<?= htmlentities($_GET["last"], ENT_QUOTES, 'UTF-8') ?>"};
+                var json_temp = {"id": <?= htmlentities($id, ENT_QUOTES, 'UTF-8') ?>, "first": "<?= htmlentities($first, ENT_QUOTES, 'UTF-8') ?>", "age":<?= $age ?>, "middle":"<?= htmlentities($middle, ENT_QUOTES, 'UTF-8') ?>", "last":"<?= htmlentities($last, ENT_QUOTES, 'UTF-8') ?>"};
             </script>
 
 
